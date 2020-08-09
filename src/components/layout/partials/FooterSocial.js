87ce217d07fc828e6313f250aca6fb344df5df65
Pradeep-M-S <pradeepms200076@gmail.com>
2020-08-09 20:09:11 +0530

    added tweaks

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -51,32 +51,48 @@ const FooterSocial = ({ className, ...props }) => {
           style={{ textAlign: "left" }}
           className="m-0 text-xs mb-16 text-left"
         >
-          ⚡ ENTRY FEE - ₹10 per player
+          ✓ Prize will distributed to both teams according to their kills as
+          said in the images above .
         </div>{" "}
         <div
           style={{ textAlign: "left" }}
           className="m-0 text-xs mb-16 text-left"
         >
-          ⚡ You can win upto <b>₹65</b>
-        </div>{" "}
-        <div
-          style={{ textAlign: "left" }}
-          className="m-0 text-xs mb-16 text-left"
-        >
-          ⚡ Prize will distributed to both teams according to their Kills
-        </div>{" "}
-        <div
-          style={{ textAlign: "left" }}
-          className="m-0 text-xs mb-16 text-left"
-        >
-          ⚡ Even if you lose, you still can win a prize amount , upto ₹20
-        </div>{" "}
-        <div
-          style={{ textAlign: "left" }}
-          className="m-0 text-xs mb-16 text-left"
-        >
-          ⚡ And losing team will also get a free entry in the weekend classic
+          ✓ And losing team will also get a free entry in the weekend classic
           match as a consolation.
+        </div>{" "}
+        <div
+          style={{ textAlign: "left" }}
+          className="m-0 text-xs mb-16 text-left"
+        >
+          ✓ Your Match timing details will be updated everyday at 5pm
+        </div>{" "}
+        <div
+          style={{ textAlign: "left" }}
+          className="m-0 text-xs mb-16 text-left"
+        >
+          ✓ Your Room ID and Password will be given before 5 mins of your Match
+          Time.
+        </div>{" "}
+        <div
+          style={{ textAlign: "left" }}
+          className="m-0 text-xs mb-16 text-left"
+        >
+          ✓ So join before your time slot, and we can maximum wait for extra 2
+          mins not more than that
+        </div>{" "}
+        <div
+          style={{ textAlign: "left" }}
+          className="m-0 text-xs mb-16 text-left"
+        >
+          ✓ IF YOUR TEAM CANNOT JOIN WITHIN THE PROVIDED TIME, YOUR TEAM WILL BE
+          DISQUALIFIED AND OPPONENT WINS THE MATCH
+        </div>{" "}
+        <div
+          style={{ textAlign: "left" }}
+          className="m-0 text-xs mb-16 text-left"
+        >
+          ✓ AND YOU WILL NOT BE REFUNDED
         </div>{" "}
         {/* <div
                     style={{ textAlign: "left" }}
