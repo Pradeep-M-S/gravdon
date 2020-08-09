df755f6d27a424cd3a50d5cabe36d7e182f810cc
Pradeep-M-S <pradeepms200076@gmail.com>
2020-08-09 16:25:27 +0530

    added footer button for registration and added a modal

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
+import Modal from "react-modal";
+import Button from "../../elements/Button";
 
 const FooterSocial = ({ className, ...props }) => {
+  const [modal2ISOpen, setModal2IsOpen] = useState(false);
+
   const classes = classNames("footer-social", className);
 
   return (
@@ -12,11 +16,90 @@ const FooterSocial = ({ className, ...props }) => {
           justifyContent: "center",
           alignItems: "center",
         }}
-        href="https://forms.app/form/5f1d87083f6ac6335302e4f3"
+        onClick={() => setModal2IsOpen(true)}
+        // href="https://forms.app/form/5f1d87083f6ac6335302e4f3"
         className="button button-primary m-4 hover:bg-red-700 button-sm"
       >
-        Register for Mon (12/08/20)
+        Register - Mon (12/08/20)
       </a>
+      <Modal
+        style={{
+          overlay: {
+            backgroundColor: "#fff",
+            backdropFilter: "blur(40px)",
+            zIndex: "10",
+          },
+          content: {
+            backgroundColor: "#151719",
+            boxShadow: "0 3px 15px black",
+            zIndex: "10",
+            border: "none",
+            borderRadius: "20px",
+          },
+        }}
+        isOpen={modal2ISOpen}
+        onRequestClose={() => setModal2IsOpen(false)}
+      >
+        <div
+          style={{ textAlign: "center" }}
+          className="m-0 text-xs mb-16 text-left"
+        >
+          <b>Details of the Match</b>
+        </div>{" "}
+        <div
+          style={{ textAlign: "left" }}
+          className="m-0 text-xs mb-16 text-left"
+        >
+          ⚡ ENTRY FEE - ₹10 per player
+        </div>{" "}
+        <div
+          style={{ textAlign: "left" }}
+          className="m-0 text-xs mb-16 text-left"
+        >
+          ⚡ You can win upto <b>₹65</b>
+        </div>{" "}
+        <div
+          style={{ textAlign: "left" }}
+          className="m-0 text-xs mb-16 text-left"
+        >
+          ⚡ Prize will distributed to both teams according to their Kills
+        </div>{" "}
+        <div
+          style={{ textAlign: "left" }}
+          className="m-0 text-xs mb-16 text-left"
+        >
+          ⚡ Even if you lose, you still can win a prize amount , upto ₹20
+        </div>{" "}
+        <div
+          style={{ textAlign: "left" }}
+          className="m-0 text-xs mb-16 text-left"
+        >
+          ⚡ And losing team will also get a free entry in the weekend classic
+          match as a consolation.
+        </div>{" "}
+        {/* <div
+                    style={{ textAlign: "left" }}
+                    className="m-0 text-xs mb-16 text-left"
+                  >
+                    TEAM TEAM I TEAM TEAM A B I A B Kills Kills I WINS WINS I 40
+                    00-20 I ₹65 ₹0 40 20-30 I ₹55 ₹10 40 30-35 I ₹50 ₹15 40 35+
+                    I ₹45 ₹20
+                  </div>{" "} */}
+        <a
+          href="https://forms.app/form/5f1d87083f6ac6335302e4f3"
+          className="button button-primary m-4 hover:bg-red-700 button-sm"
+        >
+          Register for Mon (12/08/20)
+        </a>
+        <Button
+          onClick={() => setModal2IsOpen(false)}
+          // href="https://forms.app/form/5f1d87083f6ac6335302e4f3"
+          className="button button-primary m-4 hover:bg-red-700 button-sm"
+        >
+          {" "}
+          &#10094; Back
+        </Button>
+      </Modal>
       <ul className="list-reset">
         <li>
           <a href="https://chat.whatsapp.com/DcuLcizeunnJDYA0vXhYOQ">
